eea7737ab93dcf3a507c89ae84c9e10bb9b34fc3
jaroslaw <jarek.skrzypczak@o2.pl>
2018-04-19 09:31:57 +0200

    Initial commit

---
+++ src/main/java/sample/Controller.java
@@ -0,0 +1,13 @@
+package sample;
+
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+
+    public void clickChooseFile(ActionEvent actionEvent) {
+    }
+
+    public void clickStartButton(ActionEvent actionEvent) {
+    }
+}
