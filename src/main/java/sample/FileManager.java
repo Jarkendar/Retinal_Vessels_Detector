fb4ee983c8b17df827dbe55a5d2a23f1aa448be5
jaroslaw <jarek.skrzypczak@o2.pl>
2018-04-25 12:47:43 +0200

    Change convert mat to image from rewrite pixel by pixel to use opencv method, is faster

--- src/main/java/sample/FileManager.java
+++ src/main/java/sample/FileManager.java
@@ -5,8 +5,8 @@ import javafx.scene.image.Image;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
@@ -26,19 +26,20 @@ public class FileManager {
     }
 
     public Image convertMatToImage(Mat mat) {
-        BufferedImage bufferedImage = new BufferedImage(mat.width(), mat.height(), BufferedImage.TYPE_INT_RGB);
-        for (int i = 0; i < mat.width(); i++) {
-            for (int j = 0; j < mat.height(); j++) {
-                Color color;
-                if (mat.get(j, i).length == 1) {
-//                    System.out.println(i+" "+mat.width()+" "+(i<mat.width())+" "+j+" "+mat.height()+" "+(j<mat.height()));
-                    color = new Color((int) mat.get(j, i)[0], (int) mat.get(j, i)[0], (int) mat.get(j, i)[0], 255);
-                } else {
-                    color = new Color((int) mat.get(j, i)[2], (int) mat.get(j, i)[1], (int) mat.get(j, i)[0], 255);
-                }
-                bufferedImage.setRGB(i, j, color.getRGB());
-            }
+        int type;
+
+        if (mat.channels() == 1) {
+            type = BufferedImage.TYPE_BYTE_GRAY;
+        } else {
+            Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGB2BGR);
+            type = BufferedImage.TYPE_3BYTE_BGR;
         }
+        BufferedImage bufferedImage;
+        byte[] data = new byte[mat.width() * mat.height() * (int) mat.elemSize()];
+        mat.get(0, 0, data);
+        bufferedImage = new BufferedImage(mat.width(), mat.height(), type);
+        bufferedImage.getRaster().setDataElements(0, 0, bufferedImage.getWidth(), bufferedImage.getHeight(), data);
+
         return SwingFXUtils.toFXImage(bufferedImage, null);
     }
 }
